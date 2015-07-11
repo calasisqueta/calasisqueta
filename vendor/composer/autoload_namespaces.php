384c67104bdc8f00535288f2eb93d5017d486663
Pau Pérez <saulopefa@gmail.com>
2015-07-12 01:33:48 +0200

    Update plugins

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Dotenv' => array($vendorDir . '/vlucas/phpdotenv/src'),
+    'Composer\\Installers\\' => array($vendorDir . '/composer/installers/src'),
 );
